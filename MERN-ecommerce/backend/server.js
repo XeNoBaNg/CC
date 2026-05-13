3ef386aad68b0eadb1295c5e68a4c7f649e5540a
XeNo <abhinavchavan0782@gmail.com>
2026-05-13 21:37:15 +0530

    add functionality

--- MERN-ecommerce/backend/server.js
+++ MERN-ecommerce/backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('YOUR_MONGODB_CONNECTION_STRING_HERE')
+mongoose.connect('mongodb+srv://Abhi:Abhi0782%40@cluster0.m3h6jeq.mongodb.net/?appName=Cluster0')
   .then(() => console.log("DB Connected"))
   .catch(err => console.log(err));
 
@@ -15,6 +15,22 @@ const Product = mongoose.model('Product', { name: String, price: Number });
 app.get('/api/products', async (req, res) => {
   const products = await Product.find();
   res.json(products);
+});
+
+app.post('/api/products', async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.json(newProduct);
+});
+
+app.put('/api/products/:id', async (req, res) => {
+  const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedProduct);
+});
+
+app.delete('/api/products/:id', async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted" });
 });
 
 app.post('/api/purchase', (req, res) => {
